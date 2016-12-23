d9fb4c45009cc3682a5d4828143440255e1c397f
xukexin <xukexin1988hpxcm@163.com>
2016-12-23 17:06:45 +0800

    20161223

--- src/leetcode2017/test123.java
+++ src/leetcode2017/test123.java
@@ -6,6 +6,8 @@ public class test123 {
 		System.out.println("Hello World");
 		System.out.println("another line too");
 		System.out.println("third line");
+		System.out.println("husadhjshdj");
+		yy.tt();
 	}
 
 }
---
+++ src/leetcode2017/yy.java
@@ -0,0 +1,9 @@
+package leetcode2017;
+
+public class yy {
+	
+	public static void tt(){
+		System.out.println("sdsd");
+	}
+
+}
