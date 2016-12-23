c0bdfa8c20bd08e87d735862c126ea3025560401
xukexin <xukexin1988hpxcm@163.com>
2016-12-23 16:55:08 +0800

    test3

--- src/leetcode2017/test123.java
+++ src/leetcode2017/test123.java
@@ -5,6 +5,7 @@ public class test123 {
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("another line too");
+		System.out.println("third line");
 	}
 
 }
